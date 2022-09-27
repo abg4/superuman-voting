68b8332bb42c1642a845c4679361f250c8314c9e
Alex Gaines <againes@umaproject.org>
2022-09-27 14:15:49 -0500

    Updates to enable batch voting

--- README.md
+++ README.md
@@ -17,11 +17,22 @@ node index.js
 If you would like the script to be output to a separate file, use:
 
 ```bash
-node index.js > votingParams.txt
+node index.js > votingRound_{inputRoundID}.txt
 ```
 
 In the current state, the commitVote and revealVote contract calls can easily be copied and pasted into the Snapshot Transaction JSON input. The retrieveRewards contract call needs the params that are output to be manually input as arguments.
 
-TODO: 
-1. Change the contract calls to batchCommit and batchReveal.
-2. Check with gnosis/snapshot on reason tuple is not being imported.
+### Notes on Params
+
+Timestamp: Use etherscan / events. Set timestamp param for each vote
+
+rawIdentifier: Use etherscan / events. Use https://web3-type-converter.onbrn.com/ to convert value.
+
+roundId: Find using voting contract
+
+ancillaryData: Use etherscan / events. Use https://web3-type-converter.onbrn.com/ to convert value.
+
+priceOptions
+- 0 is "NO" 
+- 1 is "YES" / "1000000000000000000". Use https://eth-converter.com/.
+- magic number is -57896044618658097711785492504343953926634992332820282019728792003956564819968
--- index.js
+++ index.js
@@ -3,74 +3,80 @@ const { padRight, utf8ToHex } = web3.utils;
 
 const { getRandomSignedInt, computeVoteHashAncillary } = require("@uma/common");
 
-// Set these params for each vote
-let account = "0x7C7a1407c35B695E4eE530D80d4bd4C1aF8569E5";
-let rawIdentifier = "ACROSS-V2";
-let ancillaryData =
-  "0x6f6f5265717565737465723a63313836666139313433353363343462326533336562653035663231383436663130343862656461";
-let roundId = "9598";
-let priceOptions = ["0"];
-let time = 1658397474;
-let votingContract = "0x8B1631ab830d11531aE83725fDa4D86012eCCd77";
-
-// helpers (no need to edit)
+// helpers (shouldn't need to edit)
+let account = "0x7C7a1407c35B695E4eE530D80d4bd4C1aF8569E5"; // never changes
+let votingContract = "0x8B1631ab830d11531aE83725fDa4D86012eCCd77"; // Use networks folder in uma repo
 let salt = getRandomSignedInt();
-let identifier = padRight(utf8ToHex(rawIdentifier), 64);
 
-// examples if you want to manually input the salt or identifier
-// let salt = "10362209928390478891184510577979174390435882974088600354474359802993390640650"
-// let identifier = "0x4143524f53532d56320000000000000000000000000000000000000000000000"
+let voting = [
+  {
+    time: 1658397474,
+    identifier: padRight(utf8ToHex("YES_OR_NO_QUERY"), 64),
+    ancillaryData:
+      "0x6f6f5265717565737465723a63313836666139313433353363343462326533336562653035663231383436663130343862656461",
+    roundId: "9628",
+    price: "1000000000000000000",
+  },
+  {
+    time: 1658397474,
+    identifier: padRight(utf8ToHex("YES_OR_NO_QUERY"), 64),
+    ancillaryData:
+      "0x6f6f5265717565737465723a63313836666139313433353363343462326533336562653035663231383436663130343862656461",
+    roundId: "9628",
+    price: "1000000000000000000",
+  },
+];
 
-function voteParams() {
-  priceOptions.map((price) => {
+async function voteParams() {
+  let commits = [];
+  let reveals = [];
+
+  voting.forEach((x) => {
     const hash = computeVoteHashAncillary({
-      price: price,
+      price: x.price,
       salt: salt,
       account: account,
-      time: time,
-      ancillaryData: ancillaryData,
-      roundId: roundId,
-      identifier: identifier,
+      time: x.time,
+      ancillaryData: x.ancillaryData,
+      roundId: x.roundId,
+      identifier: x.identifier,
     });
 
-    // The commit and reveal are easily imported into the Snapshot tx builder.
-    let commitVote = `[{"to": "${votingContract}","value": "0", "method":"commitVote(bytes32,uint256,bytes,bytes32)", "params": ["${identifier}", "${time}", "${ancillaryData}", "${hash}"], "operation":"0"},`;
+    let commitVote = `[{"identifier":"${x.identifier}","time":"${x.time}","ancillaryData":"${x.ancillaryData}","hash":"${hash}"}]`;
+    let revealVote = `[{"identifier":"${x.identifier}","time":"${x.time}","price":"${x.price}","ancillaryData":"${x.ancillaryData}","salt":"${salt}"}]`;
 
-    let revealVote = `{"to": "${votingContract}","value": "0","method":"revealVote(bytes32,uint256,int256,int256)","params": ["${identifier}", "${time}", "${price}","${salt}"],"operation":"0"}]`;
-
-    // retrieve rewards needs to manually be input for each argument into the tx builder due to an issue with tuples.
-    // make sure you are on the same network as the voting contract or the abi will not populate.
-    let retrieveRewards =
-      ' "Type":" Contract interaction",\n' +
-      ' "To (address)":"' +
-      votingContract +
-      '",\n' +
-      ' "value": "0",\n' +
-      ' "function": "retrieveRewards()",\n' +
-      ' "voterAddress (address)":"' +
-      account +
-      '",\n' +
-      ' "roundId (uint256)":"' +
-      roundId +
-      '",\n' +
-      ' "toRetrieve (tuple[])": [{"identifier":"' +
-      identifier +
-      '","time":"' +
-      time +
-      '","ancillaryData":"' +
-      ancillaryData +
-      '"}]\n';
-
-    console.log(
-      `\n\ncommitVote and revealVote params for ${price}:\n` +
-        commitVote +
-        `\n` +
-        revealVote +
-        `\n\n` +
-        retrieveRewards
-    );
-    console.log("- The salt used for commitVote: " + salt);
+    commits.push(commitVote);
+    reveals.push(revealVote);
   });
+
+  async function getOutput(data) {
+    const results = data.map((commit, i, row) => {
+      if (i === 0 && row.length === 1) {
+        // First and only one.
+        return "[" + commit + "]";
+      } else if (i === 0 && row.length > 1) {
+        // First and not the only one.
+        return "[" + commit;
+      } else if (i + 1 === row.length && row.length > 1) {
+        // Last one
+        return commit + "]";
+      } else {
+        // Not first or last.
+        return commit;
+      }
+    });
+    return results;
+  }
+
+  async function runScript() {
+    const commitData = await getOutput(commits);
+    const revealData = await getOutput(reveals);
+
+    console.log(commitData.join());
+    console.log("\n" + revealData.join());
+  }
+
+  runScript();
 }
 
 voteParams();
