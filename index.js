695801252edad65f11fad3a51859259de8d03c33
Alex Gaines <againes@umaproject.org>
2022-11-22 10:16:37 -0600

    Add code comments and update formatting

--- index.js
+++ index.js
@@ -1,75 +1,48 @@
 const web3 = require("web3");
 const { padRight, utf8ToHex } = web3.utils;
-
 const { getRandomSignedInt, computeVoteHashAncillary } = require("@uma/common");
 
-// Set these params for each vote
-let account = "0x7C7a1407c35B695E4eE530D80d4bd4C1aF8569E5";
-let rawIdentifier = "ACROSS-V2";
-let ancillaryData =
-  "0x6f6f5265717565737465723a63313836666139313433353363343462326533336562653035663231383436663130343862656461";
-let roundId = "9598";
-let priceOptions = ["0"];
-let time = 1658397474;
+// networks file (https://github.com/UMAprotocol/protocol/blob/master/packages/core/networks/1.json)
 let votingContract = "0x8B1631ab830d11531aE83725fDa4D86012eCCd77";
+// voter dapp or voting contract
+let roundId = "9657";
+// safe address (should not need to be changed)
+let safe = "0xC75aDbf2a5a6A51302c1c7cC789366ed16e1E0F3";
+// voter dapp
+let rawIdentifier = "YES_NO_QUERY";
+// voter dapp
+let ancillaryData =
+  "0x6F6F5265717565737465723A63313836666139313433353363343462326533336562653035663231383436663130343862656461";
+// price you are voting for. Should be 0, 1000000000000000000, or magic number (TODO: add value)
+let priceOptions = ["1000000000000000000"];
+// voter dapp
+let time = 1668720167;
 
 // helpers (no need to edit)
 let salt = getRandomSignedInt();
 let identifier = padRight(utf8ToHex(rawIdentifier), 64);
-
-// examples if you want to manually input the salt or identifier
-// let salt = "10362209928390478891184510577979174390435882974088600354474359802993390640650"
-// let identifier = "0x4143524f53532d56320000000000000000000000000000000000000000000000"
 
 function voteParams() {
   priceOptions.map((price) => {
     const hash = computeVoteHashAncillary({
       price: price,
       salt: salt,
-      account: account,
+      account: safe,
       time: time,
       ancillaryData: ancillaryData,
       roundId: roundId,
       identifier: identifier,
     });
 
-    // The commit and reveal are easily imported into the Snapshot tx builder.
-    let commitVote = `[{"to": "${votingContract}","value": "0", "method":"commitVote(bytes32,uint256,bytes,bytes32)", "params": ["${identifier}", "${time}", "${ancillaryData}", "${hash}"], "operation":"0"},`;
+    let commitVote = `[{"to": "${votingContract}","value": "0", "method":"commitVote(bytes32,uint256,bytes,bytes32)", "params": ["${identifier}", "${time}", "${ancillaryData}", "${hash}"], "operation":"0"}]`;
+    let revealVote = `[{"to": "${votingContract}","value": "0","method":"revealVote(bytes32,uint256,int256,int256)","params": ["${identifier}", "${time}", "${price}","${ancillaryData}","${salt}"],"operation":"0"}]`;
 
-    let revealVote = `{"to": "${votingContract}","value": "0","method":"revealVote(bytes32,uint256,int256,int256)","params": ["${identifier}", "${time}", "${price}","${salt}"],"operation":"0"}]`;
+    console.log("Voting Details: ");
+    console.log("- Voting Contract: ", votingContract);
+    console.log("- Voting Round: ", roundId);
+    console.log("- Safe Address: ", safe);
 
-    // retrieve rewards needs to manually be input for each argument into the tx builder due to an issue with tuples.
-    // make sure you are on the same network as the voting contract or the abi will not populate.
-    let retrieveRewards =
-      ' "Type":" Contract interaction",\n' +
-      ' "To (address)":"' +
-      votingContract +
-      '",\n' +
-      ' "value": "0",\n' +
-      ' "function": "retrieveRewards()",\n' +
-      ' "voterAddress (address)":"' +
-      account +
-      '",\n' +
-      ' "roundId (uint256)":"' +
-      roundId +
-      '",\n' +
-      ' "toRetrieve (tuple[])": [{"identifier":"' +
-      identifier +
-      '","time":"' +
-      time +
-      '","ancillaryData":"' +
-      ancillaryData +
-      '"}]\n';
-
-    console.log(
-      `\n\ncommitVote and revealVote params for ${price}:\n` +
-        commitVote +
-        `\n` +
-        revealVote +
-        `\n\n` +
-        retrieveRewards
-    );
-    console.log("- The salt used for commitVote: " + salt);
+    console.log(commitVote + `\n` + revealVote);
   });
 }
 
